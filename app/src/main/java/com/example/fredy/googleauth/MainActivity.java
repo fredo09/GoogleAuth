70a2cf6397df4b2ff5c12e78ec1283c1876b5572
fredo09 <fred_15@outlook.com>
2017-10-24 19:31:25 -0500

    Inicio de session con google con firebase cambiendo codigo y agregando comentarios

--- app/src/main/java/com/example/fredy/googleauth/MainActivity.java
+++ app/src/main/java/com/example/fredy/googleauth/MainActivity.java
@@ -8,11 +8,19 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
@@ -23,11 +31,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private SignInButton signInButton;
 
     private static final int RC_sIGN_IN= 777;
+
+    //variable global de firebase
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getView();
+        getIntializeFirebase();
         getgoogleClient();
         getOnClick();
     }
@@ -37,12 +52,37 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         signInButton = (SignInButton) findViewById(R.id.my_googleSignin);
     }
 
+    private void getIntializeFirebase(){
+        //Metodo que Inicializa Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        //Verifica si estamos autentificados
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            //Metodo que checara si estamos autentificados o no
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //Se ejecuta cuando cambie el estado de autentificacion
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    sucessLogin(); //Abrira la nueva activity
+                }
+            }
+        };
+    }
+    //Escuchara los cambios de estado de autentificacion
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
     public void getgoogleClient(){
         //Configurar el inicio de session para solicitar la ID del usuario Y correo electronico
         GoogleSignInOptions gso =new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)) //Pedimos el token que nos devuelve google
                 .requestEmail()
                 .build();
 
+        //Construlle el apiCliente
         googleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this,this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
@@ -61,6 +101,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         });
     }
 
+    //Intent de la api de google
     private void goLogin() {
         //Metodo que realizara el loginpor medio de google
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
@@ -81,12 +122,31 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     //Verificando que el inicio de sesion sea exitoso
     private void handleSignInResult(GoogleSignInResult result) {
         if(result.isSuccess()){
-            sucessLogin();
+            //sucessLogin(); //sin firebase
+            firebaseWithGoogle(result.getSignInAccount());
         }else{
             Toast.makeText(this, R.string.no_inicio, Toast.LENGTH_SHORT).show();
         }
     }
+    //Metodo de autentificacion con firebases
+    private void firebaseWithGoogle(GoogleSignInAccount signInAccount) {
+        //Proporcionamos el token
+        AuthCredential credencial = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
 
+
+        firebaseAuth.signInWithCredential(credencial).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                //Metodo que realiza cuando termina la peticion de autentificacion
+                if(task.isSuccessful()){
+                   //manda mensaje si no se pudo autentificar
+                    Toast.makeText(getApplicationContext(),R.string.no_firebase_auth, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    //Metodo con autentificacion sin firebase
     private void sucessLogin() {
         //Intanciando la nueva actividad
         Intent i = new Intent(this, PrincipalActivity.class);
@@ -98,5 +158,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener != null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
